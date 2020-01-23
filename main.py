6b66c74dd6fbad075e5eb2fcd70c1a22d8afa05e
mx0c <marius-bauer-2006@web.de>
2020-01-23 21:08:01 +0100

    added testing

--- main.py
+++ main.py
@@ -1,48 +1,64 @@
 from sklearn import datasets
-from . import knn
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
-training_percentage = 0.9 #90%
-
-# load the MNIST digits dataset
-mnist = datasets.load_digits()
-# Uses array from numpy for the mnist dat to convert to 32bit integer
-mnist_data = np.array(mnist.data.shape, dtype=np.int32)
-mnist_data = np.copy(mnist.data.astype(np.int32))
-
-training_samples = int(len(mnist_data)*(training_percentage))
-training_data = np.zeros((training_samples,mnist.data.shape[1]))
-training_data[:] = mnist_data[:training_samples]
-
+import random
 
 class knn():
-    def _init_(selfs, k=1):
-        self.data = []
+    def __init__(self, k=1, distanceFunc="euclidean"):
+        self.trainedData = []
         self.k = k
+        self.distanceFunc = distanceFunc
         self.lengths = []
 
     def train(self, data, label):
         for i in range(len(data)):
-            self.data.append((data[i],label[i]))
+            self.trainedData.append((data[i],label[i]))
 
     def predict(self, sample):
+        self.lengths = []
         nbs = self.getNeighbors(sample)
         labels = {}
-        for i in range(k):
-            labels[self.data[self.lengths[k][0]][1]] += 1
+        for i in range(self.k):
+            if self.trainedData[self.lengths[i][1]][1] in labels:
+                labels[self.trainedData[self.lengths[i][1]][1]] += 1
+            else:
+                labels[self.trainedData[self.lengths[i][1]][1]] = 1
+        #sort dict by value
+        labels = sorted(labels.items(), key=lambda x: x[1], reverse=True)
+        #return first item in dict
+        return next(iter(labels))
 
     def getNeighbors(self, sample):
-        for idx, tupel in enumerate(self.data):
-             self.lengths.append((self.euclidian(tupel[0], sample),idx))
-        self.lengths.sort(key=lambda x: x[0])
+        for idx, tupel in enumerate(self.trainedData):
+             self.lengths.append((self.euclidian(tupel[0], sample),idx,tupel[1]))
+        self.lengths = sorted(self.lengths, key=lambda x: x[0])
         tmp = []
         for i in range(self.k):
-            tmp.append(self.data[self.lengths[k][1]])
+            tmp.append(self.trainedData[self.lengths[self.k][1]])
         return tmp
 
-
-
     def euclidian(self, a, b):
-        return numpy.linalg.norm(a-b)
+        return np.linalg.norm(a-b)
+
+    def score(self):
+        pass
+
+# --------- Testing ---------
+import sklearn
+if int((sklearn.__version__).split(".")[1]) < 18:
+    from sklearn.cross_validation import train_test_split
+else:
+    from sklearn.model_selection import train_test_split
+
+# load the MNIST digits dataset
+mnist = datasets.load_digits()
+(trainData, testData, trainLabels, testLabels) = train_test_split(np.array(mnist.data),
+                                                                  mnist.target, test_size=0.25, random_state=42)
+_knn = knn(k=22)
+_knn.train(trainData,trainLabels)
+
+for i in range(10):
+    randomSample = random.randint(0, len(testData))
+    prediction = _knn.predict(testData[randomSample])
+    print("The number was {} and the prediction was {}".format(testLabels[randomSample],prediction[0]))
