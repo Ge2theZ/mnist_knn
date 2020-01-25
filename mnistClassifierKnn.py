95f601894d8563ad65d5bc0ced53dd8799409cdd
Giuseppe Ferrera <giuseppe.ferrera@student.reutlingen-university.de>
2020-01-25 21:58:55 +0100

    lul

--- mnistClassifierKnn.py
+++ mnistClassifierKnn.py
@@ -10,6 +10,7 @@ from sklearn import random_projection
 from skimage import exposure
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.manifold import TSNE
+from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 import numpy as np
 import imutils
@@ -20,48 +21,27 @@ from classes.utils import *
 
 import pandas as pd
 
-# read the mnist train data
-mnistData = pd.read_csv("data/train.csv")
-mnistTest = pd.read_csv("data/test.csv")
-dfMnist = pd.DataFrame(mnistData)
-dfTest = pd.DataFrame(mnistTest)
+# Any results you write to the current directory are saved as output.
+train = pd.read_csv('data/train.csv')
+submission = pd.read_csv('data/test.csv')
 
-# Split the mnist train data into a dataframe of train and validation labels
-dfVal = pd.DataFrame(mnistData).iloc[int(mnistData.shape[0] * 0.9):, :]
-dfTrain = pd.DataFrame(mnistData).iloc[:int(mnistData.shape[0] * 0.9), :]
+y_train = train['label']
+X_train = train.drop('label', axis=1)
+X_submission = submission
 
-print("dfTrain Size: ", dfTrain.shape[0])
-print("dfVal Size: ", dfVal.shape[0])
+y_train.head()
+X_submission.head()
 
-# Convert the train dataframe to an array of features and labels
-labelsTrainArr = dfTrain['label'].to_numpy()
-trainArr = dfTrain.loc[:, 'pixel0':].to_numpy()
 
-print("labelsTrainArr Size: ", labelsTrainArr.size)
-print("trainArr Elements: ", trainArr.shape[0])
+pca = PCA(n_components=50)
+X_train_transformed = pca.fit_transform(X_train)
+X_submission_transformed = pca.transform(X_submission)
 
-# Convert the val dataframe to an array of features and labels
-labelsValArr = dfVal['label'].to_numpy()
-valArr = dfVal.loc[:, 'pixel0':].to_numpy()
+X_train_pca, X_test_pca, y_train_pca, y_test_pca = train_test_split(
+    X_train_transformed, y_train, test_size=0.2, random_state=13)
+X_train_raw, X_test_raw, y_train_raw, y_test_raw = train_test_split(X_train, y_train, test_size=0.2, random_state=13)
 
-print("labelsValArr Size: ", labelsValArr.size)
-print("valArr Elements: ", valArr.shape[0])
-
-# Convert the test dataframe to an array of features
-testArr = dfTest.loc[:, 'pixel0':].to_numpy()
-print("testArr Elements: ", testArr.shape[0])
-
-# We splitted our train data into an array of train data and an array of val data. The train data is used to train our
-# classifier. The val array is used to retrieve the best value for k and to retrieve a score for our classifier.
-# The test data is data which does not contain any labels. This is the actual data that needs to be classified and
-# submitted in the kaggle contest.
-#dfTrain Size:  37800
-#dfVal Size:  4200
-#labelsTrainArr Size:  37800
-#trainArr Elements:  37800
-#labelsValArr Size:  4200
-#valArr Elements:  4200
-#testArr Elements:  28000
+print(X_train_pca.shape)
 
 trainSize = 37800
 valSize = 1000
@@ -74,10 +54,10 @@ valSize = 1000
 
 
 #(k_pca, percent) = Utils.find_k(pcaTrain, pcaVal, labelsTrainArr, labelsValArr, trainSize, valSize)
-(k_raw, percent) = Utils.find_k(trainArr, valArr, labelsTrainArr, labelsValArr, trainSize, valSize)
+(k_raw, percent) = Utils.find_k(X_train_raw, X_test_raw, y_train_raw, y_test_raw, trainSize, valSize)
 
 
-
+'''
 # re-train our classifier using the best k value and predict the labels of the
 # test data
 print("{} Reinitialize model with k={}. ".format(datetime.datetime.now(), k_raw))
@@ -110,3 +90,4 @@ for i in list(map(int, np.random.randint(0, high=valSize, size=(5,)))):
     print("I think that digit is: {}".format(prediction))
     cv2.imshow("Image", image)
     cv2.waitKey(0)
+'''
