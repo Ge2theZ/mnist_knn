e2925b745c7061ece8f25165e2c5a96fd74ed1ba
Giuseppe Ferrera <giuseppe.ferrera@student.reutlingen-university.de>
2020-01-25 22:37:03 +0100

    removed comments

--- classes/utils.py
+++ classes/utils.py
@@ -5,15 +5,15 @@ from sklearn.neighbors import KNeighborsClassifier
 import datetime
 
 class Utils():
-    def plotKAccuracieDiagram(accuracies,klist):
+    def plotKAccuracieDiagram(accuracies,klist, name):
         plt.plot(klist, accuracies)
-        plt.suptitle('K-Accuracy Diagram')
+        plt.suptitle(name)
         plt.ylabel('accuracy')
         plt.xlabel('K')
         plt.savefig('./images/kAccuracies_k1_37800samples_sklearn.png')
         plt.show()
 
-    def find_k(train, val, labelsTrain, labelsVal, trainSize, valSize):
+    def find_k(train, val, labelsTrain, labelsVal, trainSize, valSize, description):
         # initialize the values of k for our k-Nearest Neighbor classifier along with the
         # list of accuracies for each value of k
         kVals = range(1, 30, 2)
@@ -22,25 +22,25 @@ class Utils():
         for k in range(1, 30, 2):
             # train the k-Nearest Neighbor classifier with the current value of `k`
             # sklearn knn implementation
-            print("{} Initializing model with k={}. ".format(datetime.datetime.now(), k))
+            #print("{} Initializing model with k={}. ".format(datetime.datetime.now(), k))
             model = KNeighborsClassifier(n_neighbors=k)
-            print("{} Initialized model with k={}. ".format(datetime.datetime.now(), k))
+            #print("{} Initialized model with k={}. ".format(datetime.datetime.now(), k))
             # our knn implementation
             #model = knn(k=k)
-            print("{} Fitting model with k={}. ".format(datetime.datetime.now(), k))
+            #print("{} Fitting model with k={}. ".format(datetime.datetime.now(), k))
             model.fit(train[:trainSize, :], labelsTrain[:trainSize])
-            print("{} Fitted model with k={}. ".format(datetime.datetime.now(), k))
+            #print("{} Fitted model with k={}. ".format(datetime.datetime.now(), k))
 
             # evaluate the model and update the accuracies list
-            print("{} Evaluating model with k={} and a validation data set of size = {}. ".format(datetime.datetime.now(), k, valSize))
+            #print("{} Evaluating model with k={} and a validation data set of size = {}. ".format(datetime.datetime.now(), k, valSize))
             score = model.score(val[:valSize, :], labelsVal[:valSize])
-            print("{} Evaluated model with k={} and a validation data set of size = {}. ".format(datetime.datetime.now(), k, valSize))
-            print("k=%d, accuracy=%.2f%%" % (k, score * 100))
+            #print("{} Evaluated model with k={} and a validation data set of size = {}. ".format(datetime.datetime.now(), k, valSize))
+            print("{} {}: k={}, accuracy={}%".format(datetime.datetime.now(), description,k, score * 100))
             accuracies.append(score)
 
         # find the value of k that has the largest accuracy
         i = int(np.argmax(accuracies))
         print("k=%d achieved highest accuracy of %.2f%% on validation data" % (kVals[i],
                                                                                accuracies[i] * 100))
-        Utils.plotKAccuracieDiagram(accuracies,kVals)
+        Utils.plotKAccuracieDiagram(accuracies,kVals, description)
         return kVals[i], accuracies[i] * 100
